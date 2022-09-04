dd5e59c68b2c1c385f820b6dcf5e026b26ef9e0c
Houjun Liu <houjun@jemoka.com>
2022-09-04 16:10:47 -0700

    creates date time picking buttons

--- frontend/src/lib/components/DateTimePicker.svelte
+++ frontend/src/lib/components/DateTimePicker.svelte
@@ -1,5 +1,6 @@
 <script>
   import Button from "./ui/Button.svelte";
+  import DateButton from "./ui/DateButton.svelte";
   let num = 0;
 </script>
 
@@ -7,6 +8,15 @@
   <Button on:click="{()=>num += 1}" color="var(--blue)">Groot!</Button>
   <Button on:click="{()=>num += 1}" primary>Groot!</Button>
   <Button on:click="{()=>num += 1}" color="var(--green)" contrastColor="var(--black)" primary>Groot!</Button>
+
+
+
+  <DateButton on:click="{()=>num += 1}">9</DateButton>
+  <DateButton on:click="{()=>num += 1}">10</DateButton>
+  <DateButton on:click="{()=>num += 1}" color="var(--orange)">11</DateButton>
+  <DateButton on:click="{()=>num += 1}">12</DateButton>
+  <DateButton on:click="{()=>num += 1}" primary>13</DateButton>
+  <DateButton on:click="{()=>num += 1}">14</DateButton>
 </div>
 {num}
 
