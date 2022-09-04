2c79e1af257a8d8fa806bfa91802bb3fc1813c58
Houjun Liu <houjun@jemoka.com>
2022-09-04 00:04:35 -0700

    plays around with svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let testaroo = [1,2,3,4,5];  
+  let result = "";
+  $: console.log(result);
+</script>
+
+
+{#each testaroo as test (test)}
+  <!-- Only do odd numbers. -->
+  <div style="cursor:pointer" on:click="{()=>testaroo= [...testaroo, test+1]}">test! {test} {result}</div>
+{/each}
+<input placeholder="hewo" bind:value="{result}">
