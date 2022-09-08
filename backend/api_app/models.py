1ab8881e545a150922eaa89aa5db557fc1c6babe
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-09-08 14:14:22 -0700

    Remake backend project structure and create migrations

--- backend/api_app/models.py
+++ backend/api_app/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
-class User(models.model):
-    name = models.CharField()
+class User(models.Model):
+    name = models.CharField(max_length=64)
 
-class Commitment(models.model):
+class Commitment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start = models.DateTimeField()
     end = models.DateTimeField()
     is_absolute = models.BooleanField()
 
-class Meeting(models.model):
-    name = models.CharField()
+class Meeting(models.Model):
+    name = models.CharField(max_length=256)
     start = models.DateTimeField()
     end = models.DateTimeField()
     lock_in_duration = models.DurationField()
 
-class UserMeetingSubscription(models.model):
+class UserMeetingSubscription(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     meeting = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
     is_critical = models.BooleanField()
