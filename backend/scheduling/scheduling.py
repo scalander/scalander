2802804c4b3d29325d8d0810ed83dc5c3074bea9
JackHuhs <jachuhs@nuevaschool.org>
2022-09-08 15:07:20 -0700

    added classes

--- backend/scheduling/scheduling.py
+++ backend/scheduling/scheduling.py
@@ -1,21 +1,48 @@
-#IMPORTS
+# IMPORTS
+from curses import can_change_color
+from distutils.log import error
 from fcntl import F_SEAL_SEAL
 import time
 from operator import truediv
+from typing import List
+from typing_extensions import Self
 
-#FUNCTIONS
-def commitmentCheck(commitment, meeting): #if meeting and commitment intersect, return True
-    return (meeting.start>commitment.start and meeting.start<commitment.end) or (meeting.end>commitment.start and meeting.end<commitment.end)
+# CLASSES
+class User:
+    def __init__(self, name, commitments, meetingSubscriptions):
+        self.name, self.commitments, self.meetingSubscriptions = name, commitments, meetingSubscriptions
 
-def checkUserCommits(meeting, user): #check all the user's commitments with a meeting, return True if meeting time works
+class Commitment:
+    def __init__(self, start, end, isAbsolute):
+        self.start, self.end, self.isAbsolute = start, end, isAbsolute
+
+class Meeting:
+    def __init__(self, name, start, end, subscribedUsers, lockInDate):
+        self.name, self.start, self.end, self.subscribedUsers, self.lockInDate = name, start, end, subscribedUsers, lockInDate
+
+class UserMeetingSubscription:
+    def __init__(self, parent, user, meeting, isCritical, weight):
+        if parent is Meeting:
+            self.user = user
+        elif parent is User:
+            self.meeting, self.isCritical, self.weight = meeting, isCritical, weight
+        else:
+            error("Please specific parent class")
+
+
+# FUNCTIONS
+def commitment_check(commitment, meeting):  # if meeting and commitment intersect, return True
+    return (meeting.start > commitment.start and meeting.start < commitment.end) or (meeting.end > commitment.start and meeting.end < commitment.end)
+
+def check_user_commits(meeting, user):  # check all the user's commitments with a meeting, return True if meeting time works
     for c in user.commitments:
-        if commitmentCheck(c, meeting):
+        if commitment_check(c, meeting):
             return False
     return True
 
-def checkUserSubs(meeting, user): #check all the user's meeting subscriptions with a meeting, return True if meeting time works
+def check_user_subs(meeting, user):  # check all the user's meeting subscriptions with a meeting, return True if meeting time works
     for c in user.meetingSubscriptions:
-        if commitmentCheck(c.meeting, meeting):
+        if commitment_check(c.meeting, meeting):
             return False
     return True
 
