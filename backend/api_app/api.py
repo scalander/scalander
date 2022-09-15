a58ea182133524bb8d08b4697716d35e2175eb4b
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-09-15 15:29:01 -0700

    Add json object methods to API objects

--- backend/api_app/api.py
+++ backend/api_app/api.py
@@ -3,25 +3,44 @@ import api_app.models as models
 class User:
     def __init__(self, name, commitments, meeting_subscriptions):
         self.name, self.commitments, self.meeting_subscriptions = name, commitments, meeting_subscriptions
+    
+    def json_object(self):
+        return {"name": self.name, "commitments": self.commitments, "meetingSubscriptions": self.meeting_subscriptions}
 
 class Commitment:
     def __init__(self, start, end, is_absolute):
         self.start, self.end, self.is_absolute = start, end, is_absolute
+    
+    def json_object(self):
+        return {"start": self.start, "end": self.end, "isAbsolute": self.is_absolute}
 
 class Meeting:
     def __init__(self, name, start, end, proposals, subscribed_users, lock_in_date):
         self.name, self.start, self.end, self.proposals, self.subscribed_users, self.lock_in_date = name, start, end, proposals, subscribed_users, lock_in_date
+    
+    def json_object(self):
+        return {"name": self.name, "start": self.start, "end": self.end, "proposals": self.proposals, "subscribedUsers": self.subscribed_users, "lockInDate": self.lock_in_date}
 
 class MeetingTimeProposal:
     def __init__(self, start, end, commited_users, unavailable_users, optimality):
         self.start, self.end, self.commited_users, self.unavailable_users, self.optimality = start, end, commited_users, unavailable_users, optimality
+    
+    def json_object(self):
+        return {"start": self.start, "end": self.end, "commitedUsers": self.commited_users, "unavailableUsers": self.unavailable_users, "optimality": self.optimality}
 
 class MeetingAttendee:
     def __init__(self, user, is_critical, weight):
         self.user, self.is_critical, self.weight = user, is_critical, weight
+
+    def json_object(self):
+        return {"user": self.user, "isCritical": self.is_critical, "weight": self.weight}
+
 class UserAttendance:
     def __init__(self, meeting, is_critical, weight):
         self.meeting, self.is_critical, self.weight = meeting, is_critical, weight
+    
+    def json_object(self):
+        return {"meeting": self.meeting, "isCritical": self.is_critical, "weight": self.weight}
 
 def get_user(id):
     user = models.User.objects.filter(id=id).first()
