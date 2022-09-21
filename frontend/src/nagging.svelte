e5cf8ef96d4c05b31c4b5128154a29cea35ee066
Evie Wu <jonwu06@gmail.com>
2022-09-21 14:27:51 -0700

    [NW] branch and create nagging

---
+++ frontend/src/nagging.svelte
@@ -0,0 +1,34 @@
+<script>
+let invitedUsers = [{
+    name: "bob",
+},
+{
+    name: "joe"
+}] //invited people - currently hardcoded as a placeholder
+
+
+let proposals = [{
+    start: 1663794021, // proposed start time
+    end: 1663798021, // proposed end time
+    commitedUsers: [UserMeetingSubscription], // commited users
+    unavailableUsers: [UserMeetingSubscription], // to nag users
+    optimality: int // 0 means the best solution
+}]
+
+
+let meeting = { //currently filled with placeholders - eventually we'll take this in
+    name: "talk about beans",
+    start: 1663794021, // possible RANGE start as a Unix timestamp hardcoded as a placeholder
+    end: 1663994021, // possible RANGE end as a Unix timestamp hardcoded as a placeholder
+    proposals: [MeetingTimeProposal],
+    subscribedUsers: [UserMeetingSubscription],
+    lockInDate: Date, // after this date, meeting can't be rescheduled
+}
+
+</script>
+
+<main>
+
+
+
+</main>
