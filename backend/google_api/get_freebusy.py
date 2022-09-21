1f008eedff460879c5adef3f3d0ad66da9a4e288
Evie Wu <jonwu06@gmail.com>
2022-09-21 13:22:49 -0700

    [NW] add defaults to get_freebusy

--- backend/google_api/get_freebusy.py
+++ backend/google_api/get_freebusy.py
@@ -2,28 +2,36 @@
 #TODO: figure out how to test this
 from calendar_api.calendar_api import google_calendar_api
 import json
+import datetime
+today = datetime.today.strftime("%Y-%m-%d")
 g = google_calendar_api()
-calendar = g.list() #gets the list of calendars that the user is subscribed to as an object
+calendar = g.list() #gets the list of calendars that the user is subscribed to as a dictionary
 cal_ids = [] #for storing the "id" field of all the users calendars
 formatted_ids = [] #for storing the calendar IDs in the format that query() takes
-query_max = "2022-10-30T00:00:00Z" #latest date-time to check for freebusy - currently a placeholder but will eventually be user input (as specified by RFC3339 https://www.rfc-editor.org/rfc/rfc3339#section-5.6)
-query_min = "2022-9-1T00:00:00Z" #eariliest date-time to check - see query_max
+query_max = today #latest date-time to check for freebusy - default is midnight 1 week from today (as specified by RFC3339 https://www.rfc-editor.org/rfc/rfc3339#section-5.6)
+query_min = today + datetime.timedelta(days = 7) #eariliest date-time to check - default is midnight today, see query_max TODO: make this 1 week from today
 
-for cal in calendar.items: #appends all calendar IDs to cal_id
-    cal_ids.append(cal.id)
+def format_all(cals = calendar): #takes in a list of the user's calendars - default is all
+    #takes in a set of the user's calendars from the google api and formats and returns them to be used in a freebusy query - use get_freebusy()
+    for cal in cals.items: #appends all calendar IDs to cal_id
+        cal_ids.append(cal["id"])
 
-for id in cal_ids: #adds calendar IDs to formatted_ids in the format needed in query body
-    formatted_ids.append({
-        "id": id
+    for id in cal_ids: #adds calendar IDs to formatted_ids in the format needed in query body
+        formatted_ids.append({
+            "id": id
+        })
+    return(formatted_ids)
+
+
+def get_freebusy(formatted_ids = formatted_ids, query_max = query_max, query_min = query_min):
+    freebusy = g.query(body = {
+        "calendarExpansionMax": 50, # Maximal number of calendars for which FreeBusy information is to be provided. Optional. Maximum value is 50.
+        "groupExpansionMax": 100, # Maximal number of calendar identifiers to be provided for a single group. Optional. An error is returned for a group with more members than this value. Maximum value is 100.
+        "timeMax": query_max, # The end of the interval for the query formatted as per RFC3339. default is 1 week from today
+        "items": formatted_ids, #IDs of the user's calendars - use format_all() to get these
+        "timeMin": query_min, # The start of the interval for the query formatted as per RFC3339. default is today
+        "timeZone": "UTC", # Time zone used in the response. Optional. The default is UTC.
+    #structure for query body from: https://google-api-client-libraries.appspot.com/documentation/calendar/v3/python/latest/calendar_v3.freebusy.html
     })
-
-g.query(body = {
-    "calendarExpansionMax": 50, # Maximal number of calendars for which FreeBusy information is to be provided. Optional. Maximum value is 50.
-    "groupExpansionMax": 100, # Maximal number of calendar identifiers to be provided for a single group. Optional. An error is returned for a group with more members than this value. Maximum value is 100.
-    "timeMax": query_max, # The end of the interval for the query formatted as per RFC3339.
-    "items": formatted_ids,
-    "timeMin": query_min, # The start of the interval for the query formatted as per RFC3339.
-    "timeZone": "UTC", # Time zone used in the response. Optional. The default is UTC.
-#structure for query body from: https://google-api-client-libraries.appspot.com/documentation/calendar/v3/python/latest/calendar_v3.freebusy.html
-})
-
+    return (freebusy["busy"])
+    #TODO: make this return a list of commitments objects - see wiki: https://github.com/scalander/scalander/wiki/Programming-Interfaces#commitment-model
