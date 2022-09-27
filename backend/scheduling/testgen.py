4690a3ce0f2b5f40071ddf2c258aac1b2c90df58
JackHuhs <jachuhs@nuevaschool.org>
2022-09-26 17:35:12 -0700

    commit

--- backend/scheduling/testgen.py
+++ backend/scheduling/testgen.py
@@ -3,12 +3,14 @@ import os
 import datetime
 import time
 import random
+from webbrowser import MacOSX
 
 seed = 1234567890
 
 random.seed(seed)
 
 jsonData = {
+    "seed": seed,
     "iBlocks": [],
     "iMeetingLength": random.randint(10, 90),
     "iMeetingLockInDate": datetime.datetime.now().isoformat(timespec="minutes"),
@@ -21,14 +23,14 @@ stsave = st
 for i in range(random.randint(1, 15)):
     start = st  # set start of block
     st += datetime.timedelta(minutes=random.randint(jsonData["iMeetingLength"], 719))  # increment between the start and the end of the block
-    jsonData["iBlocks"].append({"start":start, "end":st})  # set end of block
+    jsonData["iBlocks"].append({"start":start.isoformat(timespec="minutes"), "end":st.isoformat(timespec="minutes")})  # set end of block
     st += datetime.timedelta(minutes=random.randint(jsonData["iMeetingLength"], 719))  # increment between end of block and start of next block
 
 def gen_commits(sta, end):  # essentially repeatedly add commitments or free time until it is past the end date
     commitments = []
     ct = sta
     while ct < end:
-        if random.choose([True, False]):  # if true add a commitment, if false make it free time
+        if random.choice([True, False]):  # if true add a commitment, if false make it free time
             temp = ct
             ct += datetime.timedelta(minutes=random.randint(1, 90))
             commitments.append({"start":temp, "end":ct})
@@ -37,10 +39,12 @@ def gen_commits(sta, end):  # essentially repeatedly add commitments or free tim
     return commitments
 
 for i in range(random.randint(2, 100)):
-    crit = random.choose([True, False])
-    weight = random.random()
-    user = {"name": f"{random.choose(['John', 'Sara', 'Abigail', 'Charlie', 'Joe', 'Frank', 'José', 'Robert', 'Elizabeth', 'Elijah', 'Theodore'])} {random.choose(['Johnson', 'Hughes', 'Smith', 'Doe', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis'])}"}
-
+    crit = random.choice([True, False])
+    weight = random.random() * 10
+    user = {"name": f"{random.choice(['John', 'Sara', 'Abigail', 'Charlie', 'Joe', 'Frank', 'José', 'Robert', 'Elizabeth', 'Elijah', 'Theodore'])} {random.choice(['Johnson', 'Andersson', 'Smith', 'Doe', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis'])}"}
+    user["commitments"] = list(map(lambda c: {"start":c["start"].isoformat(timespec="minutes"), "end":c["end"].isoformat(timespec="minutes"), "isAbsolute":random.choice([True, False])}, gen_commits(stsave, st)))
+    user["meetingSubscriptions"] = list(map(lambda m: {"meeting":{"name":" ", "start":m["start"].isoformat(timespec="minutes"), "end":m["end"].isoformat(timespec="minutes"), "subscribedUsers":[], "lockInDate":stsave.isoformat(timespec="minutes")}, "isCritical":random.choice([True, False]), "weight":random.random()*10}, gen_commits(stsave, st)))
+    jsonData["iAttendees"].append({"user":user, "isCritical":crit, "weight":weight})
 
 
 
