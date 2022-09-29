688217d76fc8298bb0328ce6378519f0bcea139b
JackHuhs <jachuhs@nuevaschool.org>
2022-09-29 13:56:20 -0700

    commit 100 tests

--- backend/scheduling/master.py
+++ backend/scheduling/master.py
@@ -1,2 +1,3 @@
-exec(open("backend/scheduling/testgen.py").read())
-exec(open("backend/scheduling/scheduling.py").read())
+for i in range(100):
+    exec(open("backend/scheduling/testgen.py").read())
+    exec(open("backend/scheduling/scheduling.py").read())
