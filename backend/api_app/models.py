92b2558c87bf705c04b82e24d879b9853d498058
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-12 13:12:52 -0700

    Allow null foreign keys

--- backend/api_app/models.py
+++ backend/api_app/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     email = models.CharField(max_length=64)
 
 class Commitment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     start = models.DateTimeField()
     end = models.DateTimeField()
     is_absolute = models.BooleanField()
@@ -17,18 +17,18 @@ class Meeting(models.Model):
     lock_in_duration = models.DurationField()
 
 class MeetingTimeProposal(models.Model):
-    meeting = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    meeting = models.ForeignKey(Meeting, null=True, on_delete=models.DO_NOTHING)
     start = models.DateField()
     end = models.DateField()
     optimality = models.IntegerField()
 
 class UserMeetingSubscription(models.Model):
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    meeting = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, null=True, on_delete=models.DO_NOTHING)
+    meeting = models.ForeignKey(Meeting, null=True, on_delete=models.DO_NOTHING)
     is_critical = models.BooleanField()
     weight = models.IntegerField()
 
 class MeetingProposalAttendance(models.Model):
-    proposal = models.ForeignKey(MeetingTimeProposal, on_delete=models.DO_NOTHING)
-    user_subscription = models.ForeignKey(UserMeetingSubscription, on_delete=models.DO_NOTHING)
+    proposal = models.ForeignKey(MeetingTimeProposal, null=True, on_delete=models.DO_NOTHING)
+    user_subscription = models.ForeignKey(UserMeetingSubscription, null=True, on_delete=models.DO_NOTHING)
     is_committed = models.BooleanField()
