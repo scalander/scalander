707da7fa994e3bc80a9e4b39e0779ab7e38c2535
Houjun Liu <houjun@jemoka.com>
2022-10-12 23:42:11 -0700

    creates actually working auth

--- frontend/src/routes/schedule/[uid]/+page.svelte
+++ frontend/src/routes/schedule/[uid]/+page.svelte
@@ -17,25 +17,21 @@
   let change = []
   import { onMount } from 'svelte';
 
+  // the button
+  let button;
+
   onMount(()=>{
-    window.onload = function () {
-      console.log("test!!");
-      google.accounts.id.initialize({
-        client_id: '966652940331-g1ld879a9gn25rk39dhta87u0trjasp3.apps.googleusercontent.com',
-        callback: ()=>{}
-      });
-      google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"),
-        { size: "small" } 
-      );
+    console.log("test!!");
+    google.accounts.id.initialize({
+      client_id: '966652940331-g1ld879a9gn25rk39dhta87u0trjasp3.apps.googleusercontent.com',
+      callback: ()=>{}
+    });
+    google.accounts.id.renderButton(
+      button,
+      { size: "large" } 
+    );
 
-    };
   });
-
-
-  // onMount(async () => {
-  //   console.log("hewo");
-  // });
 
 
 </script>
@@ -43,48 +39,16 @@
 <!-- UID: {$page.params.uid} -->
 
 
-<div id="buttonDiv" style:background="transparent"/>
-
-<!-- <div id="g_id_onload" -->
-<!--      data-client_id="{CLIENT_ID_TODO}" -->
-<!--      data-auto_prompt="false"> -->
-  <!-- </div> -->
-<!-- <div class="g_id_signin" -->
-<!--      data-type="standard" -->
-<!--      data-size="large" -->
-<!--      data-theme="outline" -->
-<!--      data-text="sign_in_with" -->
-<!--      data-shape="rectangular" -->
-<!--      data-logo_alignment="left"> -->
-  <!-- </div> -->
-<!-- data-callback="test" -->
-<!-- <div id="g_id_onload" -->
-<!--      data-client_id="966652940331-g1ld879a9gn25rk39dhta87u0trjasp3.apps.googleusercontent.com" -->
-<!--      data-context="signin" -->
-<!--      data-ux_mode="popup" -->
-<!--      data-itp_support="true" -->
-<!--      data-auto_prompt="false"> -->
-  <!-- </div> -->
-
-<!-- <div class="g_id_signin" -->
-<!--      data-type="standard" -->
-<!--      data-shape="rectangular" -->
-<!--      data-theme="outline" -->
-<!--      data-text="signin_with" -->
-<!--      data-size="large" -->
-<!--      data-logo_alignment="left"> -->
-  <!-- </div> -->
-
+<span bind:this={button} id="button"/>
 
 <DateTimeRangePicker on:change="{(e)=> change=e.detail.selected}"/>
 
-<!-- {change} -->
 
 
 
 
 <style>
-  .L5Fo6c-bF1uUb {
-      display: none;
+  #button>div {
+      display: inline;
   }
 </style>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-    plugins: [sveltekit()],
-    server: {
-        host: "0.0.0.0",
-        port: 8080,
-    }
+  plugins: [sveltekit()],
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+  },
+  optimizeDeps: { exclude: ['layercake'] }
 };
 
 export default config;
