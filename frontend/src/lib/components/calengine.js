b1657f3790b5069b4a5286bccb2094317e9fae93
Houjun Liu <houjun@jemoka.com>
2022-10-16 00:07:31 -0700

    [NW] creates date gap finder

--- frontend/src/lib/components/calengine.js
+++ frontend/src/lib/components/calengine.js
@@ -66,6 +66,102 @@ function calendarPaddingHelper(month, year, mondayWeeks = false) {
   return [beginPaddingDates, middleDates, endPaddingDates];
 }
 
-export { calendarPaddingHelper };
+// https://stackoverflow.com/questions/39469064/find-holes-gaps-in-array-of-date-ranges
+function getGapsStartRangesArray(start, end, ranges) {
+    let chunks = [],
+        i = 0, len = ranges.length, range;
+
+    let _start = start;
+
+    // If there are no ranges cached, create one big chunk for entire range.
+    if (!len) {
+        chunks.push({start: start, end: end});
+    } else {
+
+        for (; i < len; i++) {
+            range = ranges[i];
+
+            // Cache is complete or start is higher then end, we can stop looping
+            if (range.start >= end || (range.start <= start && range.end >= end)) {
+                _start = end;
+                break;
+            }
+
+            // Range hasn't gotten to start date yet, so continue
+            if (range.end < start)
+                continue;
+
+            // This range is lower then the current _start time, so we can go ahead to its end time
+            if (range.start <= _start) {
+                _start = range.end;
+            }
+            // This range is higher then the current _start time, so we are missing a piece
+            else {
+                console.log('missing piece', new Date(_start), new Date(range.start));
+                chunks.push({
+                    start: _start,
+                    end: range.start
+                });
+                _start = range.end;
+            }
+        }
+
+        // Final piece (if required)
+        if (_start < end) {
+            chunks.push({
+                start: _start,
+                end: end
+            });
+        }
+    }
+
+    return chunks;
+}
+
+function freebusyHelper(freebusy, calChoices=[]) {
+    // helper to calculate freebusy info based on calendar choices
+
+    // optionally filter by calChoices
+    if (calChoices.length > 0) {
+        // TODO refactor but basicaly this is a dictionary filter
+        freebusy = Object.fromEntries(Object.entries(freebusy).filter(([k,v]) => calChoices.includes(v.calendar_name)));
+    }
+
+    // create an array to tally all busy info
+    let global_busy = [];
+
+    // get busy info for every calendar
+    for (const [_,v] of Object.entries(freebusy)) {
+        // get the ranges for which the user is busy
+        let busy_info = v.busy;
+        // convert all the dates to unix time (ms)
+        busy_info = Object.fromEntries(Object.entries(freebusy).map(([k,v]) =>
+            [k, new Date(v).getTime()])); // cast each entry to Unix time
+        // tally
+        global_busy = global_busy+busy_info;
+    }
+    // sort by start
+    global_busy.sort(x=>x.start);
+    
+    // create today and 60 days from now
+    // TODO perhaps make this not hard-coded? but given the DB
+    // hard codes 60 days, I think this is fine
+    let start_date = new Date();
+    let end_date = new Date(start_date.getFullYear(),
+                            start_date.getMonth(),
+                            start_date.getDate()+60);
+
+    // get gaps
+    let gaps = getGapsStartRangesArray(start_date.getTime(),
+                                       end_date.getTime(),
+                                       global_busy);
+
+    // serialize gaps and return
+    return gaps.map(i=>[new Date(i.start()),
+                        new Date(i.end())]);
+}
+
+
+export { calendarPaddingHelper,freebusyHelper };
 
 
