c1a2ff3c1487da3ef7b6a93fc6994c8fc11fc4fb
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-15 23:23:46 -0700

    Fix email field name in api get_user

--- backend/api_app/api.py
+++ backend/api_app/api.py
@@ -58,7 +58,7 @@ def get_user(id):
     user = models.User.objects.filter(id=id).first()
     commitment_ids = list(map(lambda com: com.id, models.Commitment.objects.filter(user_id=id).all()))
     meeting_subscription_ids = list(map(lambda sub: sub.id, models.UserMeetingSubscription.objects.filter(user_id=id).all()))
-    return User(user.name, user.emails, commitment_ids, meeting_subscription_ids)
+    return User(user.name, user.email, commitment_ids, meeting_subscription_ids)
 
 def update_user(id, obj):
     user = models.User.objects.filter(id=id).first()
