bb75a5d6426b3c81dd5dda12ded132e2014b9c0c
Evie Wu <jonwu06@gmail.com>
2022-10-15 22:24:15 -0700

    create input field component, style things

---
+++ frontend/src/lib/components/MeetingInputField.svelte
@@ -0,0 +1,8 @@
+<script>
+    //chrono
+    import * as chrono from 'chrono-node';
+
+    //return user response
+    export let response
+
+</script>
--- frontend/src/routes/create/[meetingid]/+page.svelte
+++ frontend/src/routes/create/[meetingid]/+page.svelte
@@ -1,10 +1,12 @@
-<svelte:head>
+<!-- <svelte:head>
     <script src="https://accounts.google.com/gsi/client" on:load={loadGoogle} />
-</svelte:head>
+</svelte:head> -->
 
 <!-- base copied from ./schedule/[uid] -->
-
 <script>
+    //chrono
+    import * as chrono from 'chrono-node';
+
     // page info and svelte tooling
     import { page } from '$app/stores';
     import { onMount } from 'svelte';
@@ -16,99 +18,37 @@
     // strings
     import strings from "$lib/strings.json";
     import { exclude_internal_props } from 'svelte/internal';
+    import { parseISO } from 'date-fns';
 
-    // current changes
-    let change = []
+    //state 0 (doen't exist yet) = begin meeting creation screen
+    //state 1 = form
+    //state 2 = confirmation screen (will probably come later)
+    let state = 1
+    let name
+    let start
+    let end
+    let users = []
+    let lockIn
+    let meeting
 
-    // the button
-    let gbutton;
-
-    // global state
-    // state 1 is "google or manual" screen
-    // state 2 is the datepicker + submit screen
-    // state 3 is the completion screen
-    let state = 1;
-    // wheather google is ready
-    let ready = false;
-
-    // handle credential input
-    function handleCredential(authResult) {
-        // TODO pass it to the server
-        console.log(`amazing physics going on with ${authResult.access_token}`);
-        // move on
-        state=2
+    function submit(name, start, end, users, lockIn){ //name = string, start = datetime, end = datetime, users = [User], lockIn = datetime
+        pass
     }
 
-    // submit result
-    function submitResult() {
-        // TODO pass it to hte server
-        console.log(`amazing physics going on with ${change}`);
-        // move on
-        state=3
-    }
-
-    onMount(loadGoogle);
-
-    let client;
-
-    // Initialize Google Credential Services
-    function loadGoogle() {
-        try {
-            client = google.accounts.oauth2.initTokenClient({
-                client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-                callback: handleCredential,
-                scope: "https://www.googleapis.com/auth/calendar.readonly "
-            });
-            ready = true;
-        } catch (e) {
-            console.log(e);
-            // TODO WARN
-            // this is usually when the API is not ready yet
-        }
-    };
 </script>
 
 
 <div id="page-container">
-    <div id="schedule-container"
-         class="schedule-container-{state!=2?'small':'large'}">
-        <!-- State 1 is a "what do you want to do" screen -->
+    <div id="meeting-form">
+        <!-- State 1 is the form -->
         {#if state==1}
-            <h1>{strings.SCHEDULE_FIND_TIME}</h1>
-            <p>{strings.SCHEDULE_DESCRIPTION}</p>
-            <div class="schedule-action-container">
-                <div id="schedule-action-buttons">
-                    {#if ready}
-                    <Button primary
-                            on:click={()=>client.requestAccessToken()}>
-                        {strings.SCHEDULE_READ_CAL}</Button>
-                    or
-                    {/if}
-                    <Button primary
-                            on:click={()=>state=2}>
-                        {strings.SCHEDULE_PICK_MANUALLY}</Button>
-                </div>
-            </div>
-        {:else if state == 2}
-            <div>
-                <div>
-                    <span class="action"
-                           on:click={()=>state=1}>
-                        <i class="fa-solid fa-chevron-left action" />
-                        {strings.GLOBAL_BACK}
-                    </span>
-                    <span class="action right"
-                          on:click={submitResult}><i class="fa-solid fa-check action"></i> {strings.GLOBAL_DONE}</span>
-                </div>
-                <DateTimeRangePicker on:change="{(e)=> change=e.detail.selected}"/>
-            </div>
-        {:else}
-            <div class="schedule-center-container">
-                <div>
-                    <h1>🎉 {strings.SCHEDULE_CONGRATS}</h1>
-                    <p style="font-weight: 500; padding-top: 10px">{strings.SCHEDULE_CONGRATS_MSG}</p>
-                </div>
-            </div>
+            <h1>{strings.CREATE_A_MEETING}</h1>
+            <form class = "meeting-form"> <!-- TODO: make a thing for input fields bc listing all of them here is hella messy -->
+                <label for = "field-label">{strings.MEETING_NAME}</label>
+                <input type = "text" bind:value={name} />
+                <label for = "field-label">{strings.MEETING_START}</label>
+                <input type = "text" bind:value={name} />
+            </form>
         {/if}
     </div>
 </div>
@@ -123,55 +63,25 @@
         align-items: center;
     }
 
-    #schedule-container {
-        background: var(--accented-background);
-        border-radius: 5px;
-        padding: 20px;
-    }
-    .schedule-container-small {
-        width: min(80vw, 500px);
-        height: min(80vh, 250px);
-    }
-    .schedule-container-large {
-        width: min(80vw, 700px);
-        height: min(80vh, 450px);
+   
+
+    #meeting-form {
+        display: grid;
+        gap: 100px 200px;
+        grid-template-columns: 30% 70%;
+        grid-template-rows: auto auto auto auto auto;
     }
 
-    .schedule-action-container {
-        height: calc(min(80vh, 250px) - 150px);
-        justify-content: center;
-        align-items: center;
-        display: flex;
-    }
-
-    .schedule-center-container {
-        width: 100%;
-        height: 100%;
-        justify-content: center;
-        align-items: center;
-        display: flex;
-    }
-
-    #schedule-action-buttons {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 30px;
-        color: var(--accent);
+    /* .field-label {
+        font-weight: normal;
+        color: var(--primary);
+        font-size: 20px;
     }
 
     #done-button {
         float: right;
-    }
+    } */
 
-    .action {
-        color: var(--accent);
-        cursor: pointer;
-    }
-
-    .right {
-        float: right;
-    }
 
     h1 {
         font-weight: 700;
