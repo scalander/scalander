03d45df6cd77cd16a376934faac9e8ff3afdc640
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-16 00:32:34 -0700

    Fix proposal model fetch

--- backend/api_app/api.py
+++ backend/api_app/api.py
@@ -151,7 +151,7 @@ def update_meeting(id, obj):
     to_attach = set(obj.proposals) - current_proposals
     to_delete = current_proposals - set(obj.proposals)
     for proposal_id in to_attach:
-        proposal = get_proposal(proposal_id)
+        proposal = models.MeetingTimeProposal.objects.filter(id=proposal_id).first()
         proposal.user_id = id
         proposal.save()
     for proposal_id in to_delete:
