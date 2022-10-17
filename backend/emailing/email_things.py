26b7f0dda3964ee2500942282674a68d7b3b9d15
JackHuhs <jachuhs@nuevaschool.org>
2022-10-16 23:18:47 -0700

    basic plaintext email format missing api key

--- backend/emailing/email_things.py
+++ backend/emailing/email_things.py
@@ -13,28 +13,41 @@
 # print(response.body)
 # print(response.headers)
 
+
+### IMPORTS
+
 import sendgrid
 import os
+from datetime import *
+import datetime
 
-sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+
+### VARIABLES
+
+meeting = Meeting()  # I presume Theodore will add the actual meeting input
+
+
+# to = [
+#     {"email": "john@doe.com", "name": "John Doe"}, 
+#     {"email": "sara@calendlysux.xyz", "name": "Sara Doe"}
+# ]
+
+
+sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))  # still need api key
 data = {
   "personalizations": [
     {
-      "to": [
-        {
-          "email": "test@example.com"
-        }
-      ],
-      "subject": "Sending with SendGrid is Fun"
+      "to": [{"email": u.user.email, "name": u.user.name} for u in meeting.subscribedUsers],
+      "subject": f"{meeting.name} Confirmed From {meeting.start.isoformat(timespec="minutes")} To {meeting.end.isoformat(timespec="minutes")}"
     }
   ],
   "from": {
-    "email": "test@example.com"
+    "email": "test@example.com"  # whatever the email linked to sendgrid is, not sure of this yet
   },
   "content": [
     {
-      "type": "text/plain",
-      "value": "and easy to do anywhere, even with Python"
+      "type": "text/plain",  # for now just plaintext, will format more later
+      "value": f"The Meeting \"{meeting.name}\" has been scheduled to start at {meeting.start.isoformat(timespec="minutes")} and end at {meeting.end.isoformat(timespec="minutes")}, lasting a total of {(meeting.end - meeting.start).total_seconds() // 60} minutes."  # format a bit better later, including the datetime iso format
     }
   ]
 }
