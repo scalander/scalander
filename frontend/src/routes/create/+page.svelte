046f100d65c855efe7a27b906c14836448e6c477
Houjun Liu <houjun@jemoka.com>
2022-10-17 21:07:56 -0700

    meeting creation UI

--- frontend/src/routes/create/+page.svelte
+++ frontend/src/routes/create/+page.svelte
@@ -21,21 +21,11 @@
     let name;
     let start;
     let end;
-    let users; //stores emails separated by commas
-    let lockIn;
-    let meeting = {"name": "", "start": "", "end": "", "users": "", "lockIn": ""};
+    // this needs to be a ARRAY
+    // user + priority
+    let users=[["", 1]];
 
-    
-    // puts meeting info into one object
-    function meetingAppend(items){
-        // console.log(typeof item)
-        for (let i = 0; i < items.length; i++){
-            let key = Object.keys(items[i]);
-            let item = items[i];
-            meeting[key] = item[key];
-        }
-    }
-</script>
+    </script>
 
 
 <div id="page-container">
@@ -48,13 +38,58 @@
                    bind:value={name} />
 
             <h2 class="meeting-subhead">{strings.MEETING_START}</h2>
-            <input type="text" bind:value={start} />
+            <input type="text"
+                   placeholder="{strings.MEETING_START_PLACEHOLDER}"
+                   bind:value={start}
+                   on:change={()=>{
+                       // parse the date
+                       let parsed = chrono.parseDate(start, {forwardDate: true});
+                       // format the date and set to string
+                       // TODO internationalize the freedom units
+                       start = format(parsed, "EEEE, MMMM dd yyyy");
+                   }}/>
 
             <h2 class="meeting-subhead">{strings.MEETING_END}</h2>
-            <input type="text" bind:value={end} />
-
-            <h2 class="meeting-subhead">{strings.MEETING_LOCK_IN}</h2>
-            <input type="text" bind:value={lockIn} />
+            <input type="text"
+                   placeholder="{strings.MEETING_END_PLACEHOLDER}"
+                   bind:value={end}
+                   on:change={()=>{
+                       // parse the date
+                       let parsed = chrono.parseDate(end, {forwardDate: true});
+                       // format the date and set to string
+                       // TODO internationalize the freedom units
+                       end = format(parsed, "EEEE, MMMM dd yyyy");
+                   }}/>
+            <hr />
+            <h2 class="meeting-subhead"
+                style:margin="10px 0 0 0">{strings.MEETING_INVITEES}</h2>
+            <span id="priority-explanation">{strings.MEETING_PRIORITY}</span>
+            {#each users as user, i} 
+                <div class="userbox">
+                    <input type="email"
+                           class="userleft"
+                           placeholder="{strings.MEETING_EMAIL_PLACEHOLDER}"
+                           bind:value={user[0]}/>
+                    <input type="email"
+                           class="userright"
+                           placeholder="1"
+                           bind:value={user[1]}/>
+                    <div class="usericon">
+                        {#if i!=0}
+                        <i class="fa-solid fa-trash icon"
+                           on:click="{()=>{users.splice(i, 1);
+                                           // reset to trigger render
+                                           users=users;}}"></i>
+                        {/if}
+                        {#if i==(users.length-1)}
+                            <i class="fa-solid fa-plus icon"
+                               on:click="{()=>{users.push(["", 1]);
+                                               // reset to trigger render
+                                               users=users;}}"></i>
+                        {/if}
+                    </div>
+                </div>
+            {/each} 
         </form>
         <div id="submit">
             <Button primary>{strings.MEETING_SUBMIT}</Button>
@@ -64,6 +99,25 @@
 
 
 <style>
+    .userbox {
+        display: flex;
+    }
+
+    .userleft {
+        width: inherit;
+        flex-grow: 1;
+        max-width: 500px;
+        width: min(500px, 70vw);
+    }
+
+    .userright {
+        width: inherit;
+        flex-shrink: 1;
+        width: 50px;
+        text-align: center !important;
+        margin: 5px 10px;
+    }
+
     #page-container{
         width: 100vw;
         height: 100vh;
@@ -83,6 +137,14 @@
         font-size: 13px;
         font-weight: 600;
     }
+
+    #priority-explanation {
+        color: var(--secondary);
+        font-size: 12px;
+        transform: translateY(-2px);
+        display: block;
+    }
+
 
     input {
         margin-bottom: 15px;
@@ -107,6 +169,24 @@
 
     #submit {
         margin-top: 20px;
+    }
+
+    .usericon {
+        display: flex;
+        align-items: center;
+        gap: 13px;
+        font-size: 13px;
+    }
+
+    .icon {
+        color: var(--accent);
+        cursor: pointer;
+        opacity: 0.9;
+        transition: opacity 0.3s linear;
+    }
+
+    .icon:hover {
+        opacity: 1;
     }
 
 </style>
