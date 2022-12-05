bae0a535c0f552c5f5a9509fbd7418ff1780a17c
thegail <52760728+thegail@users.noreply.github.com>
2022-12-05 13:20:51 -0800

    Add test for meeting creation

--- backend/api_app/tests.py
+++ backend/api_app/tests.py
@@ -37,3 +37,17 @@ class UserTestCase(TestCase):
         self.assertEqual(len(fetched_meeting.proposals), 0)
         self.assertEqual(len(fetched_meeting.subscriptions), 0)
         self.assertEqual(fetched_meeting.length, 30)
+
+    def test_meeting_proposal_create(self):
+        now = datetime.datetime.now()
+        proposal = api.MeetingTimeProposal(None, now, now, [], [], 10000)
+        proposal_id = api.create_proposal(proposal)
+        meeting = api.Meeting("Some Meeting", now, now, now, 30, [proposal_id], [])
+        meeting_id = api.create_meeting(meeting)
+        fetched_meeting = api.get_meeting(meeting_id)
+        fetched_proposal = api.get_proposal(fetched_meeting.proposals[0])
+        self.assertEqual(fetched_proposal.start, now)
+        self.assertEqual(fetched_proposal.end, now)
+        self.assertEqual(len(fetched_proposal.committed_attendences), 0)
+        self.assertEqual(len(fetched_proposal.unavailable_attendences), 0)
+        self.assertEqual(fetched_proposal.optimality, 10000)
